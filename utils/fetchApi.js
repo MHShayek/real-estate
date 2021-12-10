3a87d1d8b1f0767ee7044c2e5c04db6975bc1adc
Mahedi Shayek <alfmhs@gmail.com>
2021-12-10 17:14:13 +0600

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "33c8c9c0c4mshe9593af5faaad1cp163da9jsnff3f02e0fcbd",
+    },
+  });
+  return data;
+};
